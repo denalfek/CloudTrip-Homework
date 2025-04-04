39501540f0538b951aecad8fa55892e7ce14a3be
Denis Sokolov <sokolovdd@ddplanet.ru>
2025-04-04 08:18:30 +0700

    initial apps with containerization; add cors policy

---
+++ CloudTrip.Homework.Server/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(opts =>
+{
+    opts.AddPolicy("CorsPolicy",
+        policy => policy
+            .WithOrigins("http://localhost:53072", "http://127.0.0.1:53072", "http://localhost:9090/")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseCors("CorsPolicy");
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
