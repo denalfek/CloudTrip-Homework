5c7341201e0f99b208d7ba49f2a44f596b4dc2ba
Denis Sokolov <sokolovdd@ddplanet.ru>
2025-04-05 12:02:49 +0700

    dto flight models

---
+++ CloudTrip.Homework.Common/Dto/FlightModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CloudTrip.Homework.Common.Dto;
+
+public class FlightModel
+{
+    public record GetAvailableFlightQuery(
+        string Origin, string Destination, DateTime DepartureDate);
+
+    public record AvailableFlight(
+        string ProviderName,
+        string FlightCode,
+        string Airline,
+        DateTime DepartureTime,
+        DateTime ArrivalTime,
+        decimal Price);
+}
